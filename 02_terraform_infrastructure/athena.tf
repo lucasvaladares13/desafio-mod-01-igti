d98bc6610a2f68250ef4455f4fd037eed6959f44
lucasvaladares13 <lucasvao96@gmail.com>
2021-08-19 00:42:32 -0300

    Add Athena table

---
+++ 02_terraform_infrastructure/athena.tf
@@ -0,0 +1,26 @@
+resource "aws_glue_catalog_table" aws_glue_desafio_mod_01_table_02" {
+  name          = "table_desafio_mod_01_v02"
+  database_name = aws_glue_catalog_database.aws_glue_desafio_mod_01_database.name
+
+  table_type = "EXTERNAL_TABLE"
+
+  parameters = {
+    EXTERNAL              = "TRUE"
+    "parquet.compression" = "SNAPPY"
+  }
+
+  storage_descriptor {
+    location      = "s3://datalake-lucas-igti-desafio-mod-01/staging-zone/matricula/"
+    input_format  = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
+
+    ser_de_info {
+      name                  = "mod_01"
+      serialization_library = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
+
+      parameters = {
+        "serialization.format" = 1
+      }
+    }
+  }
+}
